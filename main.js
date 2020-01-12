91fab9ea1efd8989706bbf90df44e97bc232c7e9
alexandre <amenezes031@gmail.com>
2020-01-12 18:29:25 -0300

    Using operators REST and SPREAD.

--- main.js
+++ main.js
@@ -1,22 +1,52 @@
-const usuario = {
-  nome: 'Diego',
-  idade: 23,
-  endereco: {
-    cidade: 'Rio do Sul',
-    estado: 'SC'
-  },
-};
+// REST operator: usado para pegar o resto das propriedades
 
-// Conceito de desestruturação
-const { nome, idade, endereco: { cidade } } = usuario;
+// const usuario = {
+//   nome: 'Diego',
+//   idade: 23,
+//   empresa: 'Rocketseat'
+// };
 
-console.log(nome);
-console.log(idade);
-console.log(cidade);
+// const { nome, ...resto} = usuario;
 
-// Usuando desestruturação em funções
-function mostraNome({ nome, idade }) {
-  console.log(nome, idade);
+// console.log(nome);
+// console.log(resto);
+
+// Aplicando rest operator em vetores
+// const arr = [1, 2, 3, 4];
+
+// const [a, b, ...c] = arr;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// Aplicando rest operator em parâmetros de função
+// function soma(...param) {
+//   return param.reduce((total, next) => total + next);
+// }
+
+function soma(a, b, ...param) {
+  return param;
 }
 
-mostraNome(usuario);
+console.log(soma(1, 3, 4, 4, 5, 6, 7));
+
+// SPREAD operator: propaga as informações de um objeto ou array e repassa para uma outra estrutura de dados
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3);
+
+// SPREAD operator: copiar propriedades de umm objeto para alterar uma propriedade específica
+const usuario1 = {
+  nome: 'Diego',
+  idade: 23,
+  empresa: 'Rocketseat',
+};
+
+const usuario2 = { ...usuario1, nome: 'Gabriel' };
+
+console.log(usuario2);
