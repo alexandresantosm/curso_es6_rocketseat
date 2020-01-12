02a16c11d69232ea49df886aeaaf71da79de061b
alexandre <amenezes031@gmail.com>
2020-01-12 15:22:50 -0300

    Constants and scope variables: const and let.

--- bundle.js
+++ bundle.js
@@ -1,26 +1,22 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+// const não é possível a reatribuição.
+// const a = 1;
+// a = 3;
+// Porém, é possível mutar uma variável const
+var usuario = {
+  nome: 'Diego'
+};
+usuario.nome = 'Cleiton';
+console.log(usuario); // let variável de escopo
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function teste(x) {
+  var y = 2;
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Matematica =
-/*#__PURE__*/
-function () {
-  function Matematica() {
-    _classCallCheck(this, Matematica);
+  if (x > 5) {
+    var _y = 4;
+    console.log(x, _y);
   }
+}
 
-  _createClass(Matematica, null, [{
-    key: "soma",
-    value: function soma(a, b) {
-      return a + b;
-    }
-  }]);
-
-  return Matematica;
-}();
-
-console.log(Matematica.soma(1, 2));
+teste(10);
--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-class Matematica {
-  static soma(a, b) {
-    return a + b;
+// const não é possível a reatribuição.
+
+// const a = 1;
+
+// a = 3;
+
+// Porém, é possível mutar uma variável const
+const usuario = { nome: 'Diego'}
+
+usuario.nome = 'Cleiton';
+
+console.log(usuario);
+
+// let variável de escopo
+
+function teste(x) {
+  let y = 2;
+
+  if (x > 5) {
+    let y = 4;
+    console.log(x, y); 
   }
 }
 
-console.log(Matematica.soma(1, 2));
+teste(10);
