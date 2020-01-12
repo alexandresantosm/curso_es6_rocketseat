b7d3e038d8b2f72ff9a7209654de520a652d84ca
alexandre <amenezes031@gmail.com>
2020-01-12 15:06:31 -0300

    Method static in Javascript

--- main.js
+++ main.js
@@ -1,30 +1,7 @@
-class List {
-  constructor() {
-    this.data = [];
-  }
-
-  add(data) {
-    this.data.push(data);
-    console.log(this.data);
+class Matematica {
+  static soma(a, b) {
+    return a + b;
   }
 }
 
-class TodoList extends List{
-  constructor() {
-    super();
-
-    this.usuario = 'Diego';
-  }
-
-  mostraUsuario() {
-    console.log(this.usuario);
-  }
-}
-
-var minhaLista = new TodoList();
-
-document.getElementById('novotodo').onclick = function() {
-  minhaLista.add('Novo todo');
-}
-
-minhaLista.mostraUsuario();
+console.log(Matematica.soma(1, 2));
